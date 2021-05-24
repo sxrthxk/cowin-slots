4e9478b34e354841e042b1ab261624a2c6991238
Sarthak Gupta <sarthak.ag2001@gmail.com>
2021-05-24 22:32:10 +0530

    Added more info to Cards

--- src/components/CenterCard.jsx
+++ src/components/CenterCard.jsx
@@ -1,4 +1,4 @@
-import { Card, CardContent, Typography } from "@material-ui/core";
+import { Card, CardContent, Divider, Typography } from "@material-ui/core";
 import React from "react";
 import useStyles from "../styles";
 
@@ -16,6 +16,16 @@ const CenterCard = ({ centerData }) => {
         <Typography>
           Time : {centerData.from} to {centerData.to}
         </Typography>
+        {centerData.sessions.map((session) => {return (<div key={session.session_id}><Typography>Vaccine : <u>{session.vaccine}</u></Typography>
+        <Typography>Age Limit : {session.min_age_limit}</Typography>
+        <Typography>Dose 1 : {session.available_capacity_dose1}</Typography>
+        <Typography>Dose 2 : {session.available_capacity_dose1}</Typography>
+        <Divider className={classes.divider}/>
+        {session.slots.map((slot,index) => {return (
+          <Typography> Slot {index + 1} : {slot}</Typography>
+        )})}
+        </div>)})}
+        
       </CardContent>
     </Card>
   );
